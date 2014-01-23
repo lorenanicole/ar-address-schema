08522ac5f50415bf6410744e8c1a6ef831a595e3
lorenanicole <me@lorenamesa.com>
2014-01-22 23:56:25 -0600

    .

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,25 @@ require_relative '../../db/config'
 
 class Contact< ActiveRecord::Base
 
-# implement your Contact model here
+  validates :email, :format => { :with => /.+@\w+\.\w{2,4}/ },
+            :uniqueness => true
+  validates :phone, :format => { :with => /[0-9]{10,}/ }
+
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.find_by_area_code(condition, value)
+    ## e.g. LIKE '%312'
+    data = $db.execute("SELECT * from contacts where phone LIKE ?",value
+    fetch_contact(data)
+  end
+
+  def self.fetch_contact(data)
+    data.map do |attributes|
+      Student.new( Hash[attributes.map { |k,v| [k.to_sym, v] }] )
+    end
+  end
 
 end
--- db/migrate/20121011144238_create_contacts.rb
+++ db/migrate/20121011144238_create_contacts.rb
@@ -4,6 +4,13 @@ require_relative '../config'
 
 class CreateContacts < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.stirng :company, null: false
+      t.string :email
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
   end
-end
